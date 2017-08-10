094bc1ea97dcefaeccbb22a9b80f4589091b24c7
Tyson Gach <tyson@tysongach.com>
2017-08-09 22:10:50 -0400

    Add registration page

---
+++ site/templates/registration.php
@@ -0,0 +1,9 @@
+<? snippet("header") ?>
+
+<h1><?= $page->title()->html() ?></h1>
+
+<a href="<?= $page->cvent_registration_url()->html() ?>" target="_blank">
+  Go to Cvent
+</a>
+
+<? snippet("footer") ?>
