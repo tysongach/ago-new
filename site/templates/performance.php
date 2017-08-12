b471b09872ef107eb8d047e1bb76007c814537cf
Tyson Gach <tyson@tysongach.com>
2017-08-12 12:46:40 -0400

    Add performers
    Performers can be selected as part of a performance.

--- site/templates/performance.php
+++ site/templates/performance.php
@@ -6,4 +6,16 @@
   &larr; Back to all <?= $page->parent()->title() ?>
 </a>
 
+<dl>
+  <dt>Performers:</dt>
+
+  <? foreach($page->performers()->toStructure() as $performer): ?>
+    <? $performer = $page->parent()->siblings()->find("performers")->find($performer) ?>
+    <dd>
+      <p><?= $performer->title() ?></p>
+      <p><?= $performer->bio() ?></p>
+    </dd>
+  <? endforeach ?>
+</dl>
+
 <? snippet("footer") ?>
