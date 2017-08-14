57d0f1defe8f847bf3144e26533e50ac078f53c8
Tyson Gach <tyson@tysongach.com>
2017-08-14 09:16:41 -0400

    Add metadata to performers

--- site/templates/performer.php
+++ site/templates/performer.php
@@ -6,6 +6,24 @@
   &larr; Back to all <?= $page->parent()->title() ?>
 </a>
 
+<? if($photo = $page->photo()->toFile()): ?>
+  <img src="<?= $photo->crop(100)->url() ?>"
+       alt="<?= $page->title()->html() ?>">
+<? endif ?>
+
+<p><?= $page->job_title() ?></p>
+<p><?= $page->workplace() ?></p>
+<p><?= $page->location() ?></p>
 <p><?= $page->bio() ?></p>
+
+<ul>
+  <? foreach($page->links()->toStructure() as $link): ?>
+    <li>
+      <a href="<?= $link->url() ?>" target="_blank">
+        <?= $link->name() ?>
+      </a>
+    </li>
+  <? endforeach ?>
+</ul>
 
 <? snippet("layout-bottom") ?>
