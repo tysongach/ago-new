a4aa3575c6f3d2ada3a8af119898de42b0276e51
Tyson Gach <tyson@tysongach.com>
2017-08-14 09:34:04 -0400

    Add venue metadata

--- site/templates/venue.php
+++ site/templates/venue.php
@@ -6,4 +6,11 @@
   &larr; Back to all <?= $page->parent()->title() ?>
 </a>
 
+<p><?= $page->location() ?></p>
+<p>
+  <a href="<?= $page->website() ?>" target="_blank">
+    Website
+  </a>
+</p>
+
 <? snippet("layout-bottom") ?>
