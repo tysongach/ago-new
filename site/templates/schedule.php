740749897cfb3cff904c685392f3c2cb8f1c5de8
Tyson Gach <tyson@tysongach.com>
2017-10-01 22:15:52 -0400

    Schedule table

--- site/templates/schedule.php
+++ site/templates/schedule.php
@@ -2,15 +2,27 @@
 
 <h1><?= $page->title() ?></h1>
 
-<ul>
-  <? foreach($ordered_items as $item): ?>
-    <li>
-      <?= $item->title() ?>
-      <em>
-        (<?= $item->date("M j, Y", "start_date") ?> <?= $item->date("g:ia ", "start_time") ?>)
-      </em>
-    </li>
-  <? endforeach ?>
-</ul>
+<table>
+  <thead>
+    <tr>
+      <th scope="col">Time</th>
+      <th scope="col">Event</th>
+      <th scope="col">Featuring</th>
+    </tr>
+  </thead>
+  <tbody>
+    <? foreach($ordered_items as $item): ?>
+      <tr>
+        <td>
+          <?= $item->date("M j, Y", "start_date") ?>
+          <?= $item->date("g:ia ", "start_time") ?>
+          <?= $item->date("g:ia ", "end_time") ?>
+        </td>
+        <td><?= $item->title() ?></td>
+        <td></td>
+      </tr>
+    <? endforeach ?>
+  </tbody>
+</table>
 
 <? snippet("layout-bottom") ?>
