6d69d9655e158d21781623a2667628dfa87c3c73
Tyson Gach <tyson@tysongach.com>
2017-11-04 21:37:56 -0400

    Add utm source to ad links

--- site/snippets/advertisement.php
+++ site/snippets/advertisement.php
@@ -1,6 +1,6 @@
 <figure class="c-advertisement">
   <a class="c-advertisement__link"
-     href="<?= $ad->destination_url() ?>"
+     href="<?= $ad->destination_url() ?>?utm_source=agokc2018"
      target="_blank"
      rel="noopener">
     <img src="<?= $ad->images()->find($ad->art())->crop(480, 320)->url() ?>"
