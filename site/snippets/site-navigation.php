ba32b899aee99f284b1c71bb24020fce0c9f9843
Tyson Gach <tyson@tysongach.com>
2017-11-07 10:32:12 -0500

    Responsive nav

--- site/snippets/site-navigation.php
+++ site/snippets/site-navigation.php
@@ -1,18 +1,20 @@
 <nav class="c-site-nav" role="navigation">
-  <? foreach($pages->visible() as $p): ?>
-    <a class="c-site-nav__link"
-       href="<?= $p->url() ?>"
-       <? ecco($p->isOpen(), ' aria-current="page"') ?>>
-      <?= $p->title() ?>
-    </a>
-  <? endforeach ?>
+  <div class="c-site-nav__link-list">
+    <? foreach($pages->visible() as $p): ?>
+      <a class="c-site-nav__link"
+         href="<?= $p->url() ?>"
+         <? ecco($p->isOpen(), ' aria-current="page"') ?>>
+        <?= $p->title() ?>
+      </a>
+    <? endforeach ?>
 
-  <a class="c-site-nav__link"
-     href="<?= $site->cvent_registration_url() ?>"
-     target="_blank"
-     rel="noopener">
-    Register
-  </a>
+    <a class="c-site-nav__link"
+       href="<?= $site->cvent_registration_url() ?>"
+       target="_blank"
+       rel="noopener">
+      Register
+    </a>
+  </div>
 
   <? snippet("ago-kc-2018-logo") ?>
 </nav>
